7b7202219d87667ef4f203dad61d9009b41c1a5a
GlobalN0mad <Mark.Cheshire@sloan.mit.edu>
2013-08-16 09:06:28 +0000

    corrected typo

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
-var fs = require(´'fs');
+var fs = require('fs');
 var buf = fs.readFileSync(index.html);
 var hello = buf.toString();
 app.get('/', function(request, response) {
