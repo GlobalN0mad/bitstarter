74fe656cfb8aa3183051f0879a5eb855d6b3560d
GlobalN0mad <Mark.Cheshire@sloan.mit.edu>
2013-08-16 09:04:26 +0000

    Require fs

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
+var fs = require(´'fs');
 var buf = fs.readFileSync(index.html);
 var hello = buf.toString();
 app.get('/', function(request, response) {
